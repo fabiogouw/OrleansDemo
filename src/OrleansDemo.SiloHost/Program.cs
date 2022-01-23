e343d1061bd57a77dccabcce24dd65a8e0eadb06
Fabio Gouw <fabiogouw@gmail.com>
2022-01-23 16:53:12 -0300

    Upgrading to Orleans 3.6.0; changing the silo host to use UseOrleans extension method;

--- src/OrleansDemo.SiloHost/Program.cs
+++ src/OrleansDemo.SiloHost/Program.cs
@@ -23,22 +23,54 @@ namespace OrleansDemo.SiloHost
                      configHost.SetBasePath(Directory.GetCurrentDirectory());
                      configHost.AddCommandLine(args);
                  })
-                 .ConfigureAppConfiguration((hostContext, configApp) =>
+                 .ConfigureAppConfiguration((hostContext, configBuilder) =>
                  {
-                     configApp.SetBasePath(Directory.GetCurrentDirectory());
-                     configApp.AddJsonFile($"appsettings.json", true);
-                     configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true);
-                     configApp.AddUserSecrets<Program>();
-                     configApp.AddCommandLine(args);
+                     configBuilder.SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile($"appsettings.json", true)
+                        .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true)
+                        .AddUserSecrets<Program>()
+                        .AddCommandLine(args);
                  })
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddLogging();
-                    services.AddHostedService<SiloHostService>();
                 })
                 .ConfigureLogging((hostContext, configLogging) =>
                 {
                     configLogging.AddConsole();
+                })
+                .UseOrleans((hostContext, orleansBuilder) =>
+                {
+                    var config = hostContext.Configuration;
+                    int portAdd = config.GetValue<int>("portadd", 0);
+                    orleansBuilder.Configure<ClusterOptions>(options =>
+                     {
+                         options.ClusterId = "dev";
+                         options.ServiceId = "OrleansDemo";
+                     })
+                    .Configure<GrainCollectionOptions>(options =>
+                    {
+                        options.CollectionAge = TimeSpan.FromMinutes(3);
+                        options.CollectionQuantum = TimeSpan.FromMinutes(2);
+                    })
+                    //.UseLocalhostClustering()
+                    .UseAdoNetClustering(options =>
+                    {
+                        options.Invariant = "System.Data.SqlClient";
+                        options.ConnectionString = config.GetConnectionString("ClusterStorage");
+                    })
+                    //.AddMemoryGrainStorage("Devices")
+                    .AddAdoNetGrainStorage("Devices", options =>
+                    {
+                        options.Invariant = "System.Data.SqlClient";
+                        options.ConnectionString = config.GetConnectionString("GrainStorage");
+                        options.UseJsonFormat = true;
+                    })
+                    .ConfigureEndpoints(siloPort: 11111 + portAdd, gatewayPort: 30000 + portAdd)
+                    //.ConfigureApplicationParts(parts => parts.AddFromApplicationBaseDirectory())
+                    .UseDashboard(options => options.Port = 8080 + portAdd)
+                    //.UseLinuxEnvironmentStatistics()
+                    .ConfigureLogging(logging => logging.AddConsole());
                 })
                 .Build();
             await host.RunAsync();
